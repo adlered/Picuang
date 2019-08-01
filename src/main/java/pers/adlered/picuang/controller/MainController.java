0217d962c04ebb57a1f6434be836da13395dfd44
Adler <1101635162@qq.com>
2019-08-01 13:29:27 +0800

    :apple: 增加了图片存储总数的服务器剩余空间显示的功能

--- src/main/java/pers/adlered/picuang/controller/MainController.java
+++ src/main/java/pers/adlered/picuang/controller/MainController.java
@@ -2,8 +2,12 @@ package pers.adlered.picuang.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.text.DecimalFormat;
 
 @Controller
 public class MainController {
@@ -14,6 +18,14 @@ public class MainController {
     @ResponseBody
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("index");
+        //图片总数计算
+        String path = ClassUtils.getDefaultClassLoader().getResource("").getPath() + "static/uploadImages/";
+        modelAndView.addObject("files", new File(path).listFiles().length);
+        //剩余空间计算
+        File diskPartition = new File("/");
+        String freePartitionSpace = new DecimalFormat("#.00").format(diskPartition.getFreeSpace() / 1073741824);
+        modelAndView.addObject("free", freePartitionSpace);
+        //限制文件大小
         modelAndView.addObject("limit", maxFileSize);
         return modelAndView;
     }
