142e0743f5713774fefb609a715d7ac9adf840db
Adler <1101635162@qq.com>
2019-11-08 10:49:29 +0800

    修复历史记录时间线划分问题

--- src/main/resources/static/js/history/histories.js
+++ src/main/resources/static/js/history/histories.js
@@ -18,12 +18,12 @@ $(function () {
                                     .then(function (dayRes) {
                                         $.each(dayRes.data, function (key, data) {
                                             var day = data;
-                                            $("#" + year + "-" + month + "-p").append("<h4>" + day + "日</h4>");
+                                            $("#" + year + "-" + month + "-p").append("<span id='" + year + "-" + month + "-" + day  + "'><h4>" + day + "日</h4></span>");
                                             axios.get('/api/list?year=' + year + '&month=' + month + '&day=' + day)
                                                 .then(function (listRes) {
                                                     $.each(listRes.data, function (key, data) {
                                                         var list = data;
-                                                        $("#" + year + "-" + month + "-p").append("<a href='" + data.path + "' target='_blank'><img src='" + data.path + "' style='width: auto; height: auto; max-width: 128px; max-height: 128px; margin: 8px 4px' class='img-thumbnail'></a>");
+                                                        $("#" + year + "-" + month + "-" + day).append("<a href='" + data.path + "' target='_blank'><img src='" + list.path + "' style='width: auto; height: auto; max-width: 128px; max-height: 128px; margin: 8px 4px' class='img-thumbnail'></a>");
                                                         if (i == 0) {
                                                             $("#histories").prepend("" +
                                                                 "<h5>历史记录根据您的IP地址（" + data.ip + "）所生成，请及时保存，IP地址更改后历史记录将丢失。</h5>");
